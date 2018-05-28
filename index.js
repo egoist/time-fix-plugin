8f0d1abc2f29e97081d18e64178e5f6f1870bd6a
EGOIST <0x142857@gmail.com>
2018-05-28 23:44:26 +0800

    apply offset in afterCompile hook

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 module.exports = class TimeFixPlugin {
   constructor(watchOffset = 11000) {
     this.watchOffset = watchOffset
-    this.offsetApplied = false
   }
 
   apply(compiler) {
     const watch = compiler.watch
-    const ctx = this
+    let watching
+    let offsetApplied
 
     compiler.watch = function () {
-      const watching = watch.apply(this, arguments)
-      watching.startTime += ctx.watchOffset
-      ctx.offsetApplied = true
+      watching = watch.apply(this, arguments)
       return watching
     }
 
+    compiler.hooks.afterCompile.tap('time-fix-plugin', () => {
+      if (watching) {
+        watching.startTime += this.watchOffset
+        offsetApplied = true
+      }
+    })
+
     compiler.hooks.done.tap('time-fix-plugin', stats => {
-      if (this.offsetApplied) {
+      if (offsetApplied) {
         stats.startTime -= this.watchOffset
       }
     })
