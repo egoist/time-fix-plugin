3d1db0fa555312e9c79ed7ebe2182b53eec66fae
Clark Du <clark.duxin@gmail.com>
2020-09-20 17:18:15 +0100

    fix(webpack): startTime in readonly in stats

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ module.exports = class TimeFixPlugin {
     // Reset time
     compiler.hooks.done.tap('time-fix-plugin', stats => {
       if (watching && !fixed) {
-        stats.startTime -= this.watchOffset
+        if (stats.compilation.startTime) {
+          stats.compilation.startTime -= this.watchOffset
+        } else {
+          stats.startTime -= this.watchOffset
+        }
         fixed = true
       }
     })
